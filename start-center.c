2bb33bd6ed7a0e0af1fca7f19c7dca4cbc36e089
Hyeonjun Lim <hjoon0510@gmail.com>
2017-12-29 23:21:31 +0900

    Added start-cetner.c file
    I wrote this file on 12-29-2017
    
    Signed-off-by: Hyeonjun Lim <hjoon0510@gmail.com>

---
+++ start-center.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main() { 
+	int i,j=0,n, a = 0; 
+	scanf("%d", &a);
+	j = j + a;
+	for(i=0;i<a / 2 + 1;i++) { 
+		for(n=0;n<=i;n++) {
+			printf(" "); 
+		} 
+		for(n=j;n>0;n--) {
+			printf("*");
+		} 
+		j=j-2;
+		printf("\n");
+	} 
+}
